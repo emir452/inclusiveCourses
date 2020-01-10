290ed512792a4f8809f4fe12f0c53aed0ac4a022
emir <emir.chesini@gmail.com>
2020-01-10 14:28:35 -0300

    agregado el controller de login y la vista parcialmente creada para iniciar seción

---
+++ inclusiveCourses/Controllers/AcountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace inclusiveCourses.Controllers
+{
+    public class AcountController : Controller
+    {
+        public ActionResult login()
+        {
+            return View();
+
+}
+        [HttpPost]
+        public IActionResult login(String email, string password)
+        {
+            if (string.IsNullOrEmpty(email) && string.IsNullOrEmpty(password))
+            {
+                Models.User user = new Models.User();
+                user.email = email;
+                user.password = password;
+              return new ProfileController(user).index();
+            }
+            return new loginErrorController().Index();
+
+
+        }
+
+    }
+}
---
+++ inclusiveCourses/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace inclusiveCourses.Models
+{
+    public class User
+    {
+        public string email { get; set;}
+        public string password { get; set;}
+
+    }
+}
